cbe1da847fdb339930f6ca49b7e0d1053bcf0b1d
lel <lelandaure@gmail.com>
2021-11-30 01:21:58 -0500

    initial commit as first flutter project

---
+++ android/app/src/main/kotlin/com/lel/bullseye/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lel.bullseye
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BullsEyeApp());
+
+class BullsEyeApp extends StatelessWidget {
+  const BullsEyeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BullsEye',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const GamePage(tittle: 'BullsEye'),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  const GamePage({Key? key, this.tittle}) : super(key: key);
+  final String? tittle;
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  bool _isAlertVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Welcome to my second app :) :P",
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
+            ),
+            TextButton(
+              onPressed: () {
+                _isAlertVisible = true;
+                _showAlert(context);
+                print("Button pressed!");
+              },
+              child: const Text(
+                'Hit me!',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    Widget okButton = TextButton(
+        child: const Text("Awesome"),
+        onPressed: () {
+          Navigator.of(context).pop();
+          _isAlertVisible = false;
+          print("Awesome pressed! $_isAlertVisible");
+        });
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Hello there!"),
+          content: const Text("This is my first pop-up."),
+          actions: <Widget>[okButton],
+          elevation: 5,
+        );
+      },
+    );
+  }
+}
